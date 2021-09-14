ff674fe1f61d36d060b6c7bb21952a7e8fae3b31
Jaewhee Seo <jstylesss2@gmail.com>
2021-09-14 00:41:36 -0700

    Added 'DemoUser Login' functionality

--- client/src/components/AuthHeader/AuthHeader.tsx
+++ client/src/components/AuthHeader/AuthHeader.tsx
@@ -5,6 +5,10 @@ import useStyles from './useStyles';
 import { Typography } from '@material-ui/core';
 import Logo from '../../Images/logo.png';
 import AuthMenu from '../AuthMenu/AuthMenu';
+import login from '../../helpers/APICalls/login';
+import { useAuth } from '../../context/useAuthContext';
+import { useSnackBar } from '../../context/useSnackbarContext';
+import { demoUser } from '../../mocks/demoUser';
 
 interface Props {
   linkTo?: string;
@@ -14,6 +18,22 @@ interface Props {
 
 const AuthHeader = ({ asideText }: Props): JSX.Element => {
   const classes = useStyles();
+  const { updateLoginContext } = useAuth();
+  const { updateSnackBarMessage } = useSnackBar();
+
+  const handleDemoUserLogin = () => {
+    const { email, password } = demoUser;
+    login(email, password).then((data) => {
+      if (data.error) {
+        updateSnackBarMessage(data.error.message);
+      } else if (data.success) {
+        updateLoginContext(data.success);
+      } else {
+        console.error({ data });
+        updateSnackBarMessage('An unexpected error occurred. Please try again');
+      }
+    });
+  };
 
   return (
     <Box className={classes.authHeader}>
@@ -30,6 +50,16 @@ const AuthHeader = ({ asideText }: Props): JSX.Element => {
         <Link to="/signup" className={classes.link}>
           <Button className={`${classes.headerBtn} ${classes.signupBtn}`} color="primary" variant="contained">
             SIGN UP
+          </Button>
+        </Link>
+        <Link to="/dashboard" className={classes.link}>
+          <Button
+            className={`${classes.headerBtn} ${classes.loginBtn}`}
+            color="primary"
+            variant="outlined"
+            onClick={handleDemoUserLogin}
+          >
+            DEMO USER LOGIN
           </Button>
         </Link>
       </Box>
--- client/src/components/AuthMenu/AuthMenu.tsx
+++ client/src/components/AuthMenu/AuthMenu.tsx
@@ -1,19 +1,24 @@
 import { useState, MouseEvent } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import Box from '@material-ui/core/Box';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { useAuth } from '../../context/useAuthContext';
+import { demoUser } from '../../mocks/demoUser';
+import { useSnackBar } from '../../context/useSnackbarContext';
+import login from '../../helpers/APICalls/login';
 import useStyles from './useStyles';
 
 const AuthMenu = (): JSX.Element => {
   const classes = useStyles();
   const history = useHistory();
+  const location = useLocation();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const { logout } = useAuth();
+  const { updateLoginContext, loggedInUser, logout } = useAuth();
+  const { updateSnackBarMessage } = useSnackBar();
 
   const handleClick = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -38,8 +43,22 @@ const AuthMenu = (): JSX.Element => {
     history.push('/signup');
   };
 
+  const handleDemoUserLogin = () => {
+    const { email, password } = demoUser;
+    login(email, password).then((data) => {
+      if (data.error) {
+        updateSnackBarMessage(data.error.message);
+      } else if (data.success) {
+        updateLoginContext(data.success);
+      } else {
+        console.error({ data });
+        updateSnackBarMessage('An unexpected error occurred. Please try again');
+      }
+    });
+  };
+
   return (
-    <Box className={classes.authMobileMenus}>
+    <Box className={`${classes.authMobileMenus} ${location.pathname === '/dashboard' && classes.displayAuthMenus}`}>
       <IconButton aria-label="show auth menu" aria-controls="auth-menu" aria-haspopup="true" onClick={handleClick}>
         <MoreHorizIcon />
       </IconButton>
@@ -55,9 +74,15 @@ const AuthMenu = (): JSX.Element => {
         }}
         getContentAnchorEl={null}
       >
-        <MenuItem onClick={handleLogin}>Login</MenuItem>
-        <MenuItem onClick={handleSignup}>Signup</MenuItem>
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        {loggedInUser ? (
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        ) : (
+          <Box>
+            <MenuItem onClick={handleLogin}>Login</MenuItem>
+            <MenuItem onClick={handleSignup}>Signup</MenuItem>
+            <MenuItem onClick={handleDemoUserLogin}>Demo User Login</MenuItem>
+          </Box>
+        )}
       </Menu>
     </Box>
   );
