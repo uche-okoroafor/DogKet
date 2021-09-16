9e9390f153a6f6ba1b6e925cee485847fa3673f2
Jaewhee Seo <jstylesss2@gmail.com>
2021-09-15 19:03:07 -0700

    Fixed some issues regarding prettier extension

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path:__dirname + '/../sample.env'});
+require("dotenv").config({ path: __dirname + "/../sample.env" });
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { app } = require("../app.js");
@@ -6,11 +6,13 @@ const { app } = require("../app.js");
 chai.should();
 chai.use(chaiHttp);
 
-describe("Ping test with logout controller", () => {  
-  it('should return 200 and message', async () => {
-    const testingMessage = { message: 'Ping test with logout controller' }
-    const res = await chai.request.agent(app).post('/auth/logout').send(testingMessage)
+describe("Ping test with logout controller", () => {
+  it("should return 200 and message", async () => {
+    const testingMessage = { message: "Ping test with logout controller" };
+    const res = await chai.request.agent(app).post("/auth/logout").send(testingMessage);
     res.should.have.status(200);
-    res.should.have.property('text').eql(`Server is running. Message received: ${testingMessage.message}`);
-  })
+    res.should.have
+      .property("text")
+      .eql(`Server is running. Message received: ${testingMessage.message}`);
+  });
 });
