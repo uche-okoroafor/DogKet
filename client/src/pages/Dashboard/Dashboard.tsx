fdd7db70803c7a44fb1ee48b3abb59c8714d281d
Alex <reyesgx11@gmail.com>
2021-09-19 23:40:28 -0700

    pr edits

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,13 @@
 import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
+import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
 import { useEffect } from 'react';
-import Bookings from '../Bookings/Bookings';
+
 
 export default function Dashboard(): JSX.Element {
   const classes = useStyles();
@@ -28,11 +30,10 @@ export default function Dashboard(): JSX.Element {
 
   return (
     <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
-      {/* <CssBaseline />
+      <CssBaseline />
       <Grid item className={classes.drawerWrapper}>
         <ChatSideBanner loggedInUser={loggedInUser} />
-      </Grid> */}
-      <Bookings />
+      </Grid>
     </Grid>
   );
 }
