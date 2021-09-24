e64c785907741f593c9fa716c0c7c4fd01dd8bc0
ashraf <ashrafeid@outlook.com>
2021-09-24 13:21:29 +0300

    remove uploadcontroller and route

--- server/routes/profileRouter.js
+++ server/routes/profileRouter.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const profileController = require("../controllers/profileController");
-const uploadController = require("../controllers/uploadController");
 
 router.post("/:id", profileController.createProfile);
 router.put("/update/:id", profileController.updateProfile);
 router.get("/:id", profileController.findProfile);
 router.get("/all:id", profileController.getAllProfiles);
-router.post("/upload:id", uploadController.setUpload);
 
 module.exports = router;
