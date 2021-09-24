e00ccbb48b5421f14e800d930cf65f6b96ffbf28
Alex <reyesgx11@gmail.com>
2021-09-24 16:21:18 -0700

    pr  ready

---
+++ server/models/Notifications.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const NotificationSchema = new mongoose.Schema({
+  userId: {
+    type: ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  type: {
+    type: String,
+    enum: [ 'newRequest', 'requestStatus', 'payment' ],
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  }, 
+  read: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Notification", NotificationSchema);
---
+++ server/routes/notifications.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const protect = require("../middleware/auth");
+
+const { 
+    createNotification,
+    notificationList,
+    updateReadStatus,
+} = require("../controllers/notifications");
+
+router.route("/create").post(protect, createNotification);
+router.route("/all").get(protect, notificationList);
+router.route("/read").patch(protect, updateReadStatus);
+
+module.exports = router;
