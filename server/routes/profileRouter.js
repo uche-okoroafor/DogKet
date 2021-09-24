6e66785013348e765e14da10306be4037fdc8133
ashraf <ashrafeid@outlook.com>
2021-09-24 13:16:26 +0300

    Resove some comments on profile controller and add profileRouter file

---
+++ server/routes/profileRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const profileController = require("../controllers/profileController");
+const uploadController = require("../controllers/uploadController");
+
+router.post("/:id", profileController.createProfile);
+router.put("/update/:id", profileController.updateProfile);
+router.get("/:id", profileController.findProfile);
+router.get("/all:id", profileController.getAllProfiles);
+router.post("/upload:id", uploadController.setUpload);
+
+module.exports = router;
