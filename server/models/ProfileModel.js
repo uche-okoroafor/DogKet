4ad74e4eadd035f3bbe7312bb46aa7b08d504a64
ashraf <ashrafeid@outlook.com>
2021-09-26 18:03:47 +0300

    add profileModel

---
+++ server/models/ProfileModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+Schema = mongoose.Schema;
+
+const profileSchema = new mongoose.Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  address: {
+    type: String,
+    minlength: 3,
+    maxlength: 200,
+  },
+  phone: {
+    type: String,
+    minlength: 5,
+    maxlength: 20,
+  },
+  description: {
+    type: String,
+    minlength: 3,
+    maxlength: 1000,
+  },
+  birth: {
+    type: String,
+    minlength: 3,
+    maxlength: 30,
+  },
+  photos: [String],
+  gender: String,
+  availability: [String],
+});
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
