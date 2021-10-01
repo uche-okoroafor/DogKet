965a75a863df086a5d0fe0087e15d1d92ca8c90f
Jaewhee Seo <jstylesss2@gmail.com>
2021-09-30 21:50:53 -0700

    Modified Profile model and controller

--- server/routes/profileRouter.js
+++ server/routes/profileRouter.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const profileController = require("../controllers/profileController");
+const { validateProfile, validateSitterProfile } = require("../validate");
 // uploadController doesn't exist in the current main branch.
 // const uploadController = require("../controllers/uploadController");
-
-router.post("/", profileController.createProfile);
-router.put("/update", profileController.updateProfile);
-router.get("/", profileController.findProfile);
-router.get("/all", profileController.getAllProfiles);
+router.post("/", validateProfile, profileController.createProfile);
+router.put(
+  "/:profileId",
+  validateProfile,
+  validateSitterProfile,
+  profileController.updateProfile
+);
+router.get("/:profileId", profileController.findProfile);
+router.get("/", profileController.getAllProfiles);
 // router.post("/upload", uploadController.setUpload);
 
 module.exports = router;
--- server/validate.js
+++ server/validate.js
@@ -1,4 +1,4 @@
-const { check, validationResult } = require("express-validator");
+const { check, validationResult, oneOf } = require("express-validator");
 
 exports.validateRegister = [
   check("username", "Please enter a username").not().isEmpty(),
@@ -29,10 +29,26 @@ exports.validateLogin = [
   },
 ];
 
-exports.validateSendMessage = [
-  check("conversationId", "conversationId is required").not().isEmpty(),
-  check("text", "text is required").not().isEmpty(),
-  check("recipientId", "recipientId is required").not().isEmpty(),
+exports.validateProfile = [
+  check("firstName", "firstName is required (min:3, max:30)").trim().isLength({
+    min: 3,
+    max: 30,
+  }),
+  check("lastName", "lastName is required (min:3, max:30)").trim().isLength({
+    min: 3,
+    max: 30,
+  }),
+  check("gender").isIn(["Male", "Female", "Other"]),
+  check("birth", "date of birth is invalid (e.g., YYYY/MM/DD)").isDate(),
+  check("phone", "phone is invalid (e.g. 123-456-7890)").isMobilePhone(),
+  check("address", "address is required (min:3, max:200)").isLength({
+    min: 3,
+    max: 200,
+  }),
+  check("description", "description is required (min:3, max:1000)").isLength({
+    min: 3,
+    max: 1000,
+  }),
   (req, res, next) => {
     const errors = validationResult(req);
 
@@ -42,8 +58,33 @@ exports.validateSendMessage = [
   },
 ];
 
-exports.validateCreateConversation = [
-  check("recipientId", "recipientId is required").not().isEmpty(),
+exports.validateSitterProfile = [
+  oneOf(
+    [
+      [
+        check("isSitter").not().isEmpty().equals("true"),
+        check("title", "title is required (min:3, max:200)")
+          .trim()
+          .isLength({
+            min: 3,
+            max: 200,
+          })
+          .toUpperCase(),
+        check("hourlyWage", "hourlyWage is required (min:1, max:500)").isInt({
+          min: 1,
+          max: 500,
+          allow_leading_zeroes: false,
+        }),
+      ],
+      [
+        check("isSitter").not().isEmpty().equals("false"),
+        check("title").isEmpty(),
+        check("hourlyWage").isEmpty(),
+      ],
+    ],
+    "title and hourlyWage are only required for Sitter profile"
+  ),
+
   (req, res, next) => {
     const errors = validationResult(req);
 
