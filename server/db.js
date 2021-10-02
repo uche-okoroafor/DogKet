c205508a5275900ef20a7062bdf7a9e793c39161
Alex <reyesgx11@gmail.com>
2021-10-02 01:55:45 -0700

    clean code

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log("connecting");
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
