b4d1e357a9cfd4baa8b2b1189830fb29bdbee311
Jaewhee Seo <jstylesss2@gmail.com>
2021-10-07 12:29:12 -0700

    Resolve merge conflict, Remove SocketProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
 import { AuthProvider } from './context/useAuthContext';
-import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
 import { ConvoProvider } from './context/useConvoContext';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
@@ -25,27 +24,25 @@ function App(): JSX.Element {
       <BrowserRouter>
         <SnackBarProvider>
           <AuthProvider>
-            <SocketProvider>
-              <Switch>
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <ProtectedRoute exact path="/dashboard" component={TempDashboardMain} />
-                <Route exact path="/listings" component={Listings} />
-                <Route path="/listings/:sitterId" component={ProfileDetail} />
-                <Route exact path="/profile" component={Profile} />
-                <Route path="/profile/:setting" component={Profile} />
-                <Route exact path="/notifications" component={Notifications} />
-                <ConvoProvider>
-                  <Route exact path="/messages" component={Messages} />
-                </ConvoProvider>
-                <Route exact path="/my-sitters" component={MySitters} />
-                <Route exact path="/my-jobs" component={MyJobs} />
-                <Route exact path="/" component={Landing} />
-                <Route path="*">
-                  <Redirect to="/login" />
-                </Route>
-              </Switch>
-            </SocketProvider>
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <ProtectedRoute exact path="/dashboard" component={TempDashboardMain} />
+              <Route exact path="/listings" component={Listings} />
+              <Route path="/listings/:sitterId" component={ProfileDetail} />
+              <Route exact path="/profile" component={Profile} />
+              <Route path="/profile/:setting" component={Profile} />
+              <Route exact path="/notifications" component={Notifications} />
+              <ConvoProvider>
+                <Route exact path="/messages" component={Messages} />
+              </ConvoProvider>
+              <Route exact path="/my-sitters" component={MySitters} />
+              <Route exact path="/my-jobs" component={MyJobs} />
+              <Route exact path="/" component={Landing} />
+              <Route path="*">
+                <Redirect to="/login" />
+              </Route>
+            </Switch>
           </AuthProvider>
         </SnackBarProvider>
       </BrowserRouter>
